ebe9c309f8a92316ee5c4bd7f383466c1af1315e
hym-dot <yumin6350@kbu.ac.kr>
2025-10-31 20:18:36 +0900

    .

--- frontend/src/pages/user/userDashboard.jsx
+++ frontend/src/pages/user/userDashboard.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import FileList from './FileList'
-import UploadForm from './UploadForm'
+import React, { useState } from "react";
+
+import UploadForm from "./UploadForm";
+import "./style/UserDashboard.scss";
+
+import UserPostList from "./UserPostList";
 const UserDashboard = () => {
+  const [search, setSearch] = useState("");
+  const [open, setOpen] = useState(false);
+
   return (
     <section>
-        <UploadForm/>
-        <FileList/>
+      <div className="inner">
+        <div className="search-wrap">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="검색어를 입력해주세요"
+          />
+          <button className="btn primary" onClick={() => setOpen(true)}>
+            업로드
+          </button>
+        </div>
+      </div>
+      <div>
+        {open && <UploadForm onClose={() => setOpen(false)} />}
+        <UserPostList search={search} />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default UserDashboard
+export default UserDashboard;
